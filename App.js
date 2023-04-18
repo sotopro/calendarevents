eb1038fe16cc55cd7685812bd98265b2abb3c6b7
Daniel Soto <dasg.1993@gmail.com>
2023-04-17 19:02:12 -0500

    [setup] - create expo app and add layout

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <TextInput placeholder='Enter your event' />
+        <Button title='Add' />
+      </View>
     </View>
   );
 }
@@ -14,7 +15,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
