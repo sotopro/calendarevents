8c9f865ff919ecdf541c5a6e2ce160501eb26f4a
Daniel Soto <dasg.1993@gmail.com>
2023-04-17 19:22:39 -0500

    [feature] -  add texinput and button in app js

--- App.js
+++ App.js
@@ -3,9 +3,9 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <View>
-        <TextInput placeholder='Enter your event' />
-        <Button title='Add' />
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Enter your event' style={styles.input} />
+        <Button title='Add' color='#52528C' />
       </View>
     </View>
   );
@@ -16,4 +16,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+  inputContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    marginTop: 55,
+    marginHorizontal: 20,
+  },
+  input: {
+    width: '70%',
+    borderBottomWidth: 1,
+    borderBottomColor: '#52528C',
+    marginRight: 10,
+    color: '#212121'
+  }
 });
