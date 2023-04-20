f29fc308548ad42fb91fe7d9c511b4b1b003a1a5
Daniel Soto <dasg.1993@gmail.com>
2023-04-19 19:08:47 -0500

    [feature] - add states to input value and event list and implement

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
+import { Text, View, Button, TextInput } from 'react-native';
 import { styles } from './styles';
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [events, setEvents] = useState([]);
+
+  const onAddEvent = () => {
+    if (text.length === 0) return;
+    setEvents([
+      ...events,
+      {
+        id: Math.random().toString(),
+        value: text
+      }
+    ]);
+    setText('');
+  }
+
+  console.warn('events', events)
+
+
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder='Enter your event' style={styles.input} />
-        <Button title='Add' color='#52528C' />
+        <TextInput 
+          placeholder='Enter your event' 
+          style={styles.input}
+          value={text}
+          onChangeText={(text) => setText(text)}
+        />
+        <Button title='Add' color='#52528C' onPress={onAddEvent}/>
       </View>
       <View style={styles.listContainer}>
         <View style={styles.itemContainer}>
